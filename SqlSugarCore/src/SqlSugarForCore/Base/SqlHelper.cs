6e2544ff642255337d1e0ec3484a49a041a13b61
sunkaixuan <610262374@qq.com>
2016-09-19 21:43:59 +0800

    ok

--- SqlSugarCore/src/SqlSugarForCore/Base/SqlHelper.cs
+++ SqlSugarCore/src/SqlSugarForCore/Base/SqlHelper.cs
@@ -21,30 +21,43 @@ namespace SqlSugar
         /// <summary>
         /// 是否清空SqlParameters
         /// </summary>
-        public bool isClearParameters = true;
-        public int CommandTimeOut = 30000;
+        public bool IsClearParameters = true;
         /// <summary>
-        /// 将页面参数自动填充到SqlParameter []，无需在程序中指定，这种情况需要注意是否有重复参数
-        /// 例如：
-        ///     var list = db.Queryable《Student》().Where("id=@id").ToList();
-        ///     以前写法
-        ///     var list = db.Queryable《Student》().Where("id=@id", new { id=Request["id"] }).ToList();
+        /// 请求超时时间
         /// </summary>
-        public bool IsGetPageParas = false;
+        public int CommandTimeOut = 30000;
+    
+        private bool IsGetPageParas = false;
+
         public SqlHelper(string connectionString)
         {
             _sqlConnection = new SqlConnection(connectionString);
             _sqlConnection.Open();
         }
+
+        /// <summary>
+        /// 获取当前数据库连接对象
+        /// </summary>
+        /// <returns></returns>
+
         public SqlConnection GetConnection()
         {
             return _sqlConnection;
         }
+
+        /// <summary>
+        /// 开始事务
+        /// </summary>
+
         public void BeginTran()
         {
             _tran = _sqlConnection.BeginTransaction();
         }
 
+        /// <summary>
+        /// 开始事务
+        /// </summary>
+        /// <param name="iso">指定事务行为</param>
         public void BeginTran(IsolationLevel iso)
         {
             _tran = _sqlConnection.BeginTransaction(iso);
@@ -60,6 +73,9 @@ namespace SqlSugar
             _tran = _sqlConnection.BeginTransaction(iso, transactionName);
         }
 
+        /// <summary>
+        /// 回滚事务
+        /// </summary>
         public void RollbackTran()
         {
             if (_tran != null)
@@ -68,6 +84,10 @@ namespace SqlSugar
                 _tran = null;
             }
         }
+
+        /// <summary>
+        /// 提交事务
+        /// </summary>
         public void CommitTran()
         {
             if (_tran != null)
@@ -76,38 +96,101 @@ namespace SqlSugar
                 _tran = null;
             }
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public string GetString(string sql, object pars)
         {
             return GetString(sql, SqlSugarTool.GetParameters(pars));
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public string GetString(string sql, params SqlParameter[] pars)
         {
             return Convert.ToString(GetScalar(sql, pars));
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public int GetInt(string sql, object pars)
         {
             return GetInt(sql, SqlSugarTool.GetParameters(pars));
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public int GetInt(string sql, params SqlParameter[] pars)
         {
             return Convert.ToInt32(GetScalar(sql, pars));
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public Double GetDouble(string sql, params SqlParameter[] pars)
         {
             return Convert.ToDouble(GetScalar(sql, pars));
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public decimal GetDecimal(string sql, params SqlParameter[] pars)
         {
             return Convert.ToDecimal(GetScalar(sql, pars));
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public DateTime GetDateTime(string sql, params SqlParameter[] pars)
         {
             return Convert.ToDateTime(GetScalar(sql, pars));
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public object GetScalar(string sql, object pars)
         {
             return GetScalar(sql, SqlSugarTool.GetParameters(pars));
         }
+
+        /// <summary>
+        /// 获取第一行第一列
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public object GetScalar(string sql, params SqlParameter[] pars)
         {
             SqlCommand sqlCommand = new SqlCommand(sql, _sqlConnection);
@@ -127,10 +210,24 @@ namespace SqlSugar
             sqlCommand.Parameters.Clear();
             return scalar;
         }
+
+        /// <summary>
+        /// 执行SQL返回受影响行数
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public int ExecuteCommand(string sql, object pars)
         {
             return ExecuteCommand(sql, SqlSugarTool.GetParameters(pars));
         }
+
+        /// <summary>
+        /// 执行SQL返回受影响行数
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public int ExecuteCommand(string sql, params SqlParameter[] pars)
         {
             SqlCommand sqlCommand = new SqlCommand(sql, _sqlConnection);
@@ -149,10 +246,24 @@ namespace SqlSugar
             sqlCommand.Parameters.Clear();
             return count;
         }
+
+        /// <summary>
+        /// 获取DataReader
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public SqlDataReader GetReader(string sql, object pars)
         {
             return GetReader(sql, SqlSugarTool.GetParameters(pars));
         }
+
+        /// <summary>
+        /// 获取DataReader
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public SqlDataReader GetReader(string sql, params SqlParameter[] pars)
         {
             SqlCommand sqlCommand = new SqlCommand(sql, _sqlConnection);
@@ -168,32 +279,78 @@ namespace SqlSugar
                 SqlSugarToolExtensions.RequestParasToSqlParameters(sqlCommand.Parameters);
             }
             SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
-            if (isClearParameters)
+            if (IsClearParameters)
                 sqlCommand.Parameters.Clear();
             return sqlDataReader;
         }
+
+        /// <summary>
+        /// 根据SQL获取T的集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public List<T> GetList<T>(string sql, object pars)
         {
             return GetList<T>(sql, SqlSugarTool.GetParameters(pars));
         }
+
+        /// <summary>
+        /// 根据SQL获取T的集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public List<T> GetList<T>(string sql, params SqlParameter[] pars)
         {
             var reval = SqlSugarTool.DataReaderToList<T>(typeof(T), GetReader(sql, pars), null);
             return reval;
         }
+
+        /// <summary>
+        /// 根据SQL获取T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public T GetSingle<T>(string sql, object pars)
         {
             return GetSingle<T>(sql, SqlSugarTool.GetParameters(pars));
         }
+
+        /// <summary>
+        /// 根据SQL获取T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public T GetSingle<T>(string sql, params SqlParameter[] pars)
         {
             var reval = SqlSugarTool.DataReaderToList<T>(typeof(T), GetReader(sql, pars), null).Single();
             return reval;
         }
+
+        /// <summary>
+        /// 获取DataTable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public DataTable GetDataTable(string sql, object pars)
         {
             return GetDataTable(sql, SqlSugarTool.GetParameters(pars));
         }
+
+        /// <summary>
+        /// 获取DataTable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pars"></param>
+        /// <returns></returns>
         public DataTable GetDataTable(string sql, params SqlParameter[] pars)
         {
             SqlDataAdapter _sqlDataAdapter = new SqlDataAdapter(sql, _sqlConnection);
@@ -212,6 +369,10 @@ namespace SqlSugar
             _sqlDataAdapter.SelectCommand.Parameters.Clear();
             return dt;
         }
+
+        /// <summary>
+        /// 释放数据库连接对象
+        /// </summary>
         public void Dispose()
         {
             if (_sqlConnection != null)
